a86e47ad45ecc0eb6b05c7e650e874dda98adfb2
acm-97 <alejandro97.acm@gmail.com>
2023-09-13 04:46:25 -0400

    add adpater for SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import {defineConfig} from 'astro/config'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
+import nodejs from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: 'hybrid',
+  adapter: nodejs({
+    mode: 'standalone',
+  }),
 })
